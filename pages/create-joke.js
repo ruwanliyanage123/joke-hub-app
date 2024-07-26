4d485017c9c40ad109e9db9b00fad97f89f9a24b
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-07-26 21:44:42 +0530

    Init commit

---
+++ pages/create-joke.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CreateJoke() {
+  return (
+    <div>
+      <h1>Create Joke</h1>
+    </div>
+  );
+}
---
+++ pages/read-joke.js
@@ -0,0 +1,7 @@
+export default function ReadJoke() {
+  return (
+    <div>
+      <h1>Read Joke</h1>
+    </div>
+  );
+}
