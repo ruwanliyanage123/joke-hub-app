486e2c69c6f3c8cb9a187e6d3ad1c11fb54d3159
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-07-28 09:09:23 +0530

    Add modification

--- pages/read-joke.js
+++ pages/read-joke.js
@@ -1,7 +1,19 @@
+import styles from "../styles/ReadJoke.module.css";
+
 export default function ReadJoke() {
+  const joke = {
+    id: "123",
+    title: "Funny Joke",
+    description: "This is a very funny joke.",
+    image: "https://example.com/joke.jpg",
+  };
+
   return (
-    <div>
-      <h1>Read Joke</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{joke.title}</h1>
+      <img src={joke.image} alt={joke.title} className={styles.image} />
+      <p className={styles.description}>{joke.description}</p>
+      <p className={styles.id}>ID: {joke.id}</p>
     </div>
   );
 }
