1bf2e592957db15e502c59c57c61f8d4dd53324f
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-07-30 00:53:43 +0530

    Add .env file

--- services/joke-service.js
+++ services/joke-service.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+const DELIVER_URL = `${process.env.DELIVER_PROTOCOL}://${process.env.DELIVER_HOST}:${process.env.DELIVER_PORT}/joke-deliver`;
+const SUBMIT_URL = `${process.env.SUBMIT_PROTOCOL}://${process.env.SUBMIT_HOST}:${process.env.SUBMIT_PORT}/joke-submit`;
 
 export const createJoke = async (jokeData) => {
   try {
-    const response = await axios.post(
-      "http://localhost:3001/joke-submit/createJoke",
-      jokeData
-    );
+    const response = await axios.post(`${SUBMIT_URL}/createJoke`, jokeData);
     console.log("Joke submitted successfully...", response.data);
   } catch (error) {
     console.error("Error submitting joke:", error);
@@ -15,7 +16,7 @@ export const createJoke = async (jokeData) => {
 export const getJokeByTitle = async (jokeData) => {
   try {
     const response = await axios.get(
-      `http://localhost:3005/joke-deliver/getRandomJokeByType/${jokeData}`
+      `${DELIVER_UR}/getRandomJokeByType/${jokeData}`
     );
     console.log("Joke submitted successfully...", response.data);
     return response.data;
@@ -27,9 +28,7 @@ export const getJokeByTitle = async (jokeData) => {
 
 export const getActiveTypes = async () => {
   try {
-    const response = await axios.get(
-      "http://localhost:3001/joke-submit/getActiveTypes"
-    );
+    const response = await axios.get(`${SUBMIT_URL}/getActiveTypes`);
     return response.data;
   } catch (error) {
     console.error("Error getting joke types:", error);
